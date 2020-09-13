a1cf8bd90740bb4bd08faf69f897cd165f66a40c
Jared Simpson <simpson.482@osu.edu>
2020-09-13 13:43:38 -0400

    Updated files

--- app.js
+++ app.js
@@ -14,60 +14,72 @@ async function start() {
         },
         {
             type: "input",
-            message: "What is your Role",
-            name: "getRole"
-        },
-        {
-            type: "input",
             message: "What is your Id?",
             name: "getId"
         },
         {
             type: "input",
+            message: "What is your Email?",
+            name: "getEmail"
+        },
+        {
+            type: "list",
+            name: "getRole",
+            message: "What is your Role? (Manager, Engineer, Intern)",
+            choices: ["Manager", "Engineer", "Intern"],
+        },
+        {
+            type: "input",
             message: "What is your School?",
-            name: "getSchool"
+            name: "getSchool",
+            when: (answers) => answers.getRole === 'Intern'
         },
         {
             type: "input",
             message: "What is your GitHub username?",
-            name: "getGithub"
+            name: "getGithub",
+            when: (answers) => answers.getRole === 'Engineer'
         },
         {
             type: "input",
             message: "What is your office number?",
-            name: "getOfficeNumber"
+            name: "getOfficeNumber",
+            when: (answers) => answers.getRole === 'Manager'
         },
-        ]);
+        {
+            type: "list",
+            message: "Would you like to add another employee? (Y or N)",
+            name: "getAnotherEmployee",
+            choices: ["Yes", "No"],
+        },
+        ]).then(answers => {
+            if (answers.getAnotherEmployee === "Yes") {
+                console.log('Lets get another one');
+            } else {
+                console.log('End of User Response');
+            }
+        });
 
 
-        console.log(`Here is the users response...`);
-        console.log(userResponse);
-        console.log(`end of userResponse...`);
+        // console.log(`Here is the users response...`);
+        // console.log(userResponse);
+        // console.log(`end of userResponse...`);
 
-        const myUserName = userResponse.getName;
-        const myId = userResponse.getId;
-        const myRole = userResponse.getRole;
-        const myOfficeNumber = userResponse.officeNumber;
-        const myGitHub = userResponse.getGithub;
-        const mySchool = userResponse.getSchool;
+        // const myUserName = userResponse.getName;
+        // const myId = userResponse.getId;
+        // const myEmail = userResponse.getEmail;
+        // const myRole = userResponse.getRole;
+        // const myOfficeNumber = userResponse.officeNumber;
+        // const myGitHub = userResponse.getGithub;
+        // const mySchool = userResponse.getSchool;
 
-        var myResult = (`
-## Name 
-${myUserName}
-## Employee Id
-${myId}
-## Employee Role
-${myRole}
-## Office Number
-${myOfficeNumber}
-## GitHub Username 
-${myGitHub}
-## School 
-${mySchool}
-`)
+        // console.log(`Here is the answers...`);
+        // console.log(answers);
+        // console.log(`end of userResponse...`);
 
-var writeResult = fs.writeFileSync(path.join(__dirname, 'README.md'), myResult)
-console.log("README.md file generated....")
+
+// var writeResult = fs.writeFileSync(path.join(__dirname, 'README.md'), myResult)
+// console.log("README.md file generated....")
 
 }
   
